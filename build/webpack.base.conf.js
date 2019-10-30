84ba32714c87c3253a511db4eb0c745d76c6f1ec
stanleyxu <stanleyxu@stanleyxudeMacBook-Pro.local>
2019-10-30 15:19:41 +0800

    init done

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -9,7 +9,11 @@ const HardSourceWebpackPlugin = require('hard-source-webpack-plugin')
 const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin
 const MiniCssExtractPlugin = require("mini-css-extract-plugin")
 const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 const PurifyCSS = require('purifycss-webpack')
+const PreloadWebpackPlugin = require('preload-webpack-plugin')
+const ScriptExtHtmlWebpackPlugin = require('script-ext-html-webpack-plugin')
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin')
 const glob = require('glob-all')
 const config = new Config()
 
@@ -18,11 +22,11 @@ config
   .entry('index')
     .add('./src/index.js')
     .end()
-  .mode('development')
+  .mode('production')
   .output
     .path(path.resolve(__dirname, '../dist'))
-    .filename('[name].[chunkhash:8].js')
-    .publicPath('http://localhost:7010/')
+    .filename('[name].[hash:8].js')
+    // .publicPath()
     .library('other')
     .libraryTarget('umd')
 
@@ -44,35 +48,53 @@ config.resolve.extensions.end()
 config.resolve.alias.set('react-native','react-native-web')
 
 // externals
-config.externals({
-  'react': 'react',
-  'react-dom': 'ReactDOM',
-  'react-router-dom': 'reactRouterDom',
-  'vue': 'vue',
-  'vue-router': 'vueRouter',
-})
+// config.externals({
+//   'react': 'react',
+//   'react-dom': 'ReactDOM',
+//   'react-router-dom': 'reactRouterDom',
+//   'vue': 'vue',
+//   'vue-router': 'vueRouter',
+// })
 
 // module
 config.module
   .rule('compile')
   .exclude.add(/(node_modules|bower_components)/)
   .end()
-    .test(/\.js|jsx|mjs$/)
-    .use('babel')
-    .loader('babel-loader')
+  .include.add(path.resolve(__dirname,'../src')).end()
+  .test(/\.js|jsx|mjs$/)
+  .use('babel')
+  .loader('babel-loader')
+  
+
 
 config.module.rule('less')
   .test(/\.less$/)
   .use('mini')
   .loader(MiniCssExtractPlugin.loader).options({
     hmr: process.env.NODE_ENV === 'development',
+    fallback: {
+      loader: require.resolve('style-loader'),
+      options: {
+        singleton: true
+      }
+    }
   })
   .end()
-  // .use('style')
-  // .loader('style-loader')
-  // .end()
   .use('css')
   .loader('css-loader')
+  .end()
+  .use('postcss')
+  .loader('postcss-loader')
+  .options({
+    ident: 'postcss',
+    plugins: (loader) => [
+      require('postcss-preset-env')(),
+      require('postcss-normalize')({ forceImport: true }),
+      require('postcss-cssnext')(),
+      require('postcss-import')({ root: loader.resourcePath }),
+      require('postcss-selector-namespace')({ selfSelector: ':namespace', namespace: `` })
+    ]})
   .end()
   .use('less')
   .loader('less-loader')
@@ -86,39 +108,100 @@ config.module.rule('sass')
   .use('mini')
   .loader(MiniCssExtractPlugin.loader).options({
     hmr: process.env.NODE_ENV === 'development',
-  })
-  .end()
+    fallback: {
+      loader: require.resolve('style-loader'),
+      options: {
+        singleton: true
+      }
+    }
+  }).end()
   .use('css')
-  .loader('css-loader')
-  .end()
+  .loader('css-loader').end()
+  .use('postcss')
+  .loader('postcss-loader')
+  .options({
+    ident: 'postcss',
+    plugins: (loader) => [
+      require('postcss-preset-env')(),
+      require('postcss-normalize')({ forceImport: true }),
+      require('postcss-cssnext')(),
+      require('postcss-import')({ root: loader.resourcePath }),
+      require('postcss-selector-namespace')({ selfSelector: ':namespace', namespace: `` })
+    ]}).end()
   .use('sass')
-  .loader('sass-loader')
-  .end()
+  .loader('sass-loader').end()
   .use('js2style')
-  .loader('js-to-styles-var-loader')
-  .end()
+  .loader('js-to-styles-var-loader').end()
+
+config.module.rule('images')
+  .test(/\.(png|jpg|gif)$/i)
+  .use('urlloader')
+  .loader('url-loader').options({
+    limit: 8192,
+    quality: 85,
+    name: '../dist/images/[name].[hash:8].[ext]',
+  }).end()
 
 
 // plugin
-config
+config  
   .plugin('progress')
-    .use(webpack.ProgressPlugin)
-    .end()
-  .plugin('clean')
-    .use(CleanWebpackPlugin)
-    .end()
-  .plugin('HashedModule')
-    .use(webpack.HashedModuleIdsPlugin)
-    .end()
+    .use(webpack.ProgressPlugin).end()
+  .plugin('IgnorePlugin')
+    .use(webpack.IgnorePlugin, [/^\.\/locale$/, /moment$/]).end()
+  .plugin('htmlwebpackplugin')
+    .use(HtmlWebpackPlugin, [
+      Object.assign(
+        {},
+        {
+          inject: true,
+          template: path.resolve(__dirname, '../index.html')
+        },
+        {
+          minify: {
+            removeComments: true,
+            collapseWhitespace: true,
+            removeRedundantAttributes: true,
+            useShortDoctype: true,
+            removeEmptyAttributes: true,
+            removeStyleLinkTypeAttributes: true,
+            keepClosingSlash: true,
+            minifyJS: true,
+            minifyCSS: true,
+            minifyURLs: true,
+          }
+        }
+      )
+    ]).end()
+  .plugin('PreloadWebpackPlugin')
+    .use(PreloadWebpackPlugin, [{
+      rel: 'preload',
+      as: 'script'
+    }]).end()
+  .plugin('ScriptExtHtmlWebpackPlugin')
+    .use(ScriptExtHtmlWebpackPlugin, [{
+      preload: /\.js$/,
+      defaultAttribute: 'defer'
+    }]).end()
   .plugin('HardSourceWebpackPlugin')
-    .use(HardSourceWebpackPlugin)
-    .end()
+    .use(HardSourceWebpackPlugin).end()
+  
+  .plugin('ModuleConcatenationPlugin')
+    .use(webpack.optimize.ModuleConcatenationPlugin).end()
+  .plugin('HashedModule')
+    .use(webpack.HashedModuleIdsPlugin).end()
   .plugin('Compression')
     .use(CompressionPlugin, [{
       algorithm: 'gzip',
-      test: /\.js(\?.*)?$/i,
       threshold: 10240,
-      minRatio: 0.8
+      minRatio: 0.7
+    }]).end()
+  
+  .plugin('PurifyCSS')
+    .use(PurifyCSS, [{
+      paths: glob.sync([
+        path.resolve(__dirname, '../src/*.js')
+      ])
     }]).end()
   .plugin('MiniCssExtractPlugin')
     .use(MiniCssExtractPlugin, [{
@@ -126,16 +209,53 @@ config
       chunkFilename: "[name].[contenthash:8].css"
   }]).end()
   .plugin('OptimizeCssAssetsPlugin')
-    .use(OptimizeCssAssetsPlugin).end()
-  .plugin('PurifyCSS')
-  .use(PurifyCSS, [{
-    paths: glob.sync([
-      path.resolve(__dirname, '../src/*.js')
-    ])
-  }]).end()
-  .plugin('BundleAnalyzerPlugin')
-    .use(BundleAnalyzerPlugin)
-    .end()
+    .use(OptimizeCssAssetsPlugin, [{ cssProcessorOptions: { safe: true } }]).end()
+  .plugin('HotModuleReplacementPlugin')
+    .use(webpack.HotModuleReplacementPlugin).end()
+  .plugin('clean')
+    .use(CleanWebpackPlugin).end()
+
+  // .plugin('BundleAnalyzerPlugin')
+  //   .use(BundleAnalyzerPlugin)
+  //   .end()
+
+// devServer
+config.devServer.contentBase('../dist')
+  .port(8080)
+  .inline(true)
+  .historyApiFallback(true)
+  .hot(true)
+  .compress(true)
+
+
+// nodeEnv: 'production',
+//         minimizer: [ new UglifyJSPlugin() ],
+//         namedChunks: true,
+//         runtimeChunk: {
+//             name: 'manifest'
+//         },
+//         splitChunks: {
+//             minSize: 30000,
+//             minChunks: 1,
+//             maxAsyncRequests: 5,
+//             maxInitialRequests: 3,
+//             name: false,
+//             cacheGroups: {
+//                 vendor: {
+//                     test: /[\\/]node_modules[\\/]/,
+//                     name: 'vendor',
+//                     chunks: 'initial',
+//                     reuseExistingChunk: true
+//                 }
+//             }
+//         }
+config.optimization
+  .minimizer('css')
+  .use(OptimizeCssAssetsPlugin, [{ cssProcessorOptions: { safe: true } }])
+  .end()
+  .minimizer('js')
+  .use(UglifyJSPlugin)
+  .end()
 
 
 
--- src/index.js
+++ src/index.js
@@ -1,15 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import './index.scss'
+import './index.less'
+import './other.less'
 // import Router from './src/App'
 
 const Chat = () => (
-    <div className={classNames('chat')}>
-      <div>this is Chat</div>
+    <div>
+      <div className={'box'}>this is Chat12123</div>
+      <div>123123123123</div>
+      <div>12312312</div>
     </div>
   )
 
 ReactDOM.render(
   <Chat/>,
-  document.getElementById('other')
+  document.getElementById('app')
 )
