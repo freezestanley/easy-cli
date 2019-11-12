9db7aa6fede4a15a037b3c414daf438237e69ddd
za-luoqiang <luoqiang@zhongan.io>
2019-11-12 20:30:11 +0800

    xx

--- aa/src/global.js
+++
@@ -1,7 +0,0 @@
-import ctrlApps, { globalEvent } from 'easy-mft'
-import application_info from '../config/application.json';
-
-export default new ctrlApps(application_info)
-export {
-    globalEvent
-}
